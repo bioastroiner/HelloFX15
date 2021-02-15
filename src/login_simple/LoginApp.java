76288cef8c76ee7d5a9afafa9719b68d4ca7d872
Bioastroiner <naderi81eco@gmail.com>
2021-02-15 20:20:51 +0330

    added simple logout

--- src/login_simple/LoginApp.java
+++ src/login_simple/LoginApp.java
@@ -1,9 +1,12 @@
 package login_simple;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,8 +26,22 @@ public class LoginApp extends Application {
             primaryStage.setTitle("Login App");
             primaryStage.setScene(mainWindow);
             primaryStage.show();
+
+            primaryStage.setOnCloseRequest(e -> logout(primaryStage));
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void logout(Stage stage){
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("Are you sure that you want to log out?!");
+        alert.setContentText("you have to log in again...");
+
+        if(alert.showAndWait().get() == ButtonType.OK){
+            System.out.println("logged out");
         }
     }
 }
--- src/login_simple/SceneController.java
+++ src/login_simple/SceneController.java
@@ -1,14 +1,51 @@
 package login_simple;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class SceneController {
 
+    Stage stage;
     @FXML
-    Label nameLabel;
+    private Label nameLabel;
+    @FXML
+    private Button logoutButton;
+    @FXML
+    private Button bomb_butt;
+    @FXML
+    private AnchorPane scenePane;
 
-    public void displayName(String username){
+    public void displayName(String username) {
         nameLabel.setText("Hello: " + username);
+    }
+
+    public void logout(ActionEvent event) {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("Are you sure that you want to log out?!");
+        alert.setContentText("you have to log in again...");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) scenePane.getScene().getWindow();
+            System.out.println("logged out");
+            System.exit(0);
+        }
+    }
+
+    public void showCode(ActionEvent event) {
+        Alert alert =
+        new Alert(Alert.AlertType.ERROR,
+                "Australia is not a real country ,Please Enter a Valid Country!",
+                ButtonType.OK);
+        alert.setHeaderText("Failed");
+        if(alert.showAndWait().get() == ButtonType.OK)
+            new Alert(Alert.AlertType.WARNING,"Activating all warhead to random locations...",ButtonType.APPLY).show();
     }
 }
