a43c8ed804030a8cec70b434b6298e05cd6f40ba
Bioastroiner <naderi81eco@gmail.com>
2021-02-15 18:38:09 +0330

    added css styling

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Sphere;
+
 public class Controller {
+
+    @FXML
+    private Sphere sphere;
+    private double x;
+    private double y;
+
+    public void exit(ActionEvent e) {
+        System.out.println("Good Bye!");
+        System.exit(0);
+    }
+
+    public void sayHelloWorld(ActionEvent e) {
+        System.out.println("Hello World");
+    }
+
+    public void onDragSphere(){
+        System.out.println("h");
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,8 +16,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            Scene mainWindow = new Scene(root);
             primaryStage.setTitle("Hello World");
-            primaryStage.setScene(new Scene(root));
+            primaryStage.setScene(mainWindow);
+            String css = this.getClass().getResource("sample.css").toExternalForm();
+            mainWindow.getStylesheets().add(css);
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
